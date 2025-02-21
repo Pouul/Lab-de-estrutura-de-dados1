7aeb7fe3fa601a1adaa198b62984832a420c99f9
Pouul <paulohuang04@gmail.com>
2025-02-21 12:19:16 +0000

    aula1.2

--- Lab1a/Ex3.java
+++ Lab1a/Ex3.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class Ex3 {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         double [] numeros= new double[10];
         double maior=0;
         double menor=0;
 
-        for (int i=0; i<=10; i++){
-            nums[i]=scanner.nextInt();
+        for (int i=0; i<10; i++){
+            System.out.print("coloque o numero: ");
+            numeros[i]=scanner.nextInt();
         }
-        for (int i=0; i<=10; i++){
-            for (int j=0; j<=10; j++){
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
                 if(numeros[i]>numeros[j]){
                     double aux=0;
                     aux=numeros[i];
@@ -20,8 +21,12 @@ public class Ex2 {
                 }
             }
         }
-        for (int i=0; i<=10; i++){
-            System.out.print(numeros[i]+" ");
-        }
+        System.out.println();
+
+        maior=numeros[9];
+        menor=numeros[0];
+
+        System.out.println("O maior numero é "+ maior);
+        System.out.print("O menor numero é "+ menor);
     }
 }
